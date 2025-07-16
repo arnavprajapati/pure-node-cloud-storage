83d0052212517a6009e777373515f35a260cf7e3
Arnav <arnavprajapati3101@gmail.com>
2025-07-16 13:20:27 +0530

    file upload via POST with stream events

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,45 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [directoryItems, setDirectoryItems] = useState([]);
+  const [progress, setProgress] = useState(0)
+
 
   async function getDirectoryItems() {
     const response = await fetch("http://192.168.21.114/");
     const data = await response.json();
     setDirectoryItems(data);
   }
+
+  async function onHandleChange(e) {
+    const file = await e.target.files[0]
+    console.log(file);
+    const xhr = new XMLHttpRequest()
+    xhr.open("POST", "http://192.168.21.114", true)
+    xhr.setRequestHeader("filename", file.name)
+    xhr.addEventListener("load", () => {
+      console.log(xhr.response);
+      getDirectoryItems()
+    })
+    xhr.upload.addEventListener("progress", (e) => {
+      const totalProgress = (e.loaded / e.total) * 100
+      console.log((`${totalProgress.toFixed(2)} % Uploaded`));
+      setProgress(`${totalProgress.toFixed(2)} % Uploaded`)
+    })
+    xhr.send(file)
+  }
+
   useEffect(() => {
     getDirectoryItems();
   }, []);
+
+
   return (
     <>
       <h1 className="text-2xl font-bold mb-4">My Files</h1>
+      <input type="file" onChange={(e) => {
+        onHandleChange(e);
+      }} />
+      <p>Progress: {progress}</p>
       <div className="space-y-2">
         {directoryItems.map((items, i) => (
           <div key={i}>
@@ -27,7 +54,7 @@ function App() {
           </div>
         ))}
       </div>
-  </>
+    </>
   );
 }
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
+import { createWriteStream } from 'fs'
 import { open, readdir, readFile } from 'fs/promises'
 import http from 'http'
-import  mime from 'mime-types'
+import mime from 'mime-types'
 
 const PORT = 80
 const Hostname = '0.0.0.0'
 
 const server = http.createServer(async (req, res) => {
-    if(req.url === '/favicon.ico') return "No Favicon"
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Filename");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Filename, Content-Type");
 
     const data = decodeURIComponent(req.url).split('?')
     const [url, queryString] = data
@@ -19,46 +24,89 @@ const server = http.createServer(async (req, res) => {
         queryParam[key] = value
     })
 
+
+    if (req.method === "OPTIONS") {
+        res.writeHead(204);
+        return res.end();
+    }
+
     // console.log(queryParam);
 
-    if(req.url === '/'){
-        serverDirectory(url, res)
-    }else{
-        try{
-            const readFile = await open(`./storage${decodeURIComponent(url)}`)
-            const fileStats = await readFile.stat()
-            if(fileStats.isDirectory()){
-                await readFile.close()
-                await serverDirectory(url, res)
-            }else{
-
-                const readStream = readFile.createReadStream()
-                const contentType = mime.contentType(url.slice(1))
-                console.log(contentType);
-                
-                res.setHeader('Content-Type', 'contentType')
-                res.setHeader('Content-Length', fileStats.size)
-
-                if(queryParam.action === 'download'){
-                    res.setHeader('Content-Disposition', `attachment; filename="${url.slice(1)}"`)
-                }else if(queryParam.action === 'open'){
-                    res.setHeader('Content-Disposition', "inline")
-                }
-
-                readStream.pipe(res)
-
-                readStream.on('close', async () => {
+    if (req.method === 'GET') {
+        if (req.url === '/favicon.ico') return "No Favicon"
+        if (req.url === '/') {
+            serverDirectory(url, res)
+        } else {
+            try {
+                const readFile = await open(`./storage${decodeURIComponent(url)}`)
+                const fileStats = await readFile.stat()
+                if (fileStats.isDirectory()) {
                     await readFile.close()
-                })
+                    await serverDirectory(url, res)
+                } else {
+
+                    const readStream = readFile.createReadStream()
+                    const contentType = mime.contentType(url.slice(1))
+                    console.log(contentType);
+
+                    res.setHeader('Content-Type', 'contentType')
+                    res.setHeader('Content-Length', fileStats.size)
+
+                    if (queryParam.action === 'download') {
+                        res.setHeader('Content-Disposition', `attachment; filename="${url.slice(1)}"`)
+                    } else if (queryParam.action === 'open') {
+                        res.setHeader('Content-Disposition', "inline")
+                    }
+
+                    readStream.pipe(res)
+
+                    readStream.on('close', async () => {
+                        await readFile.close()
+                    })
+                }
+            } catch (err) {
+                console.log(err.message);
+                res.end("Server not reached")
             }
-        }catch (err) {
-            console.log(err.message);
-            res.end("Server not reached")
         }
     }
+
+    else if (req.method === 'POST') {
+        const writeStream = createWriteStream(`./storage/${req.headers.filename}`);
+        req.pipe(writeStream);
+
+        writeStream.on("finish", () => {
+            res.end("File uploaded successfully");
+        });
+
+        writeStream.on("error", (err) => {
+            console.error(err);
+            res.statusCode = 500;
+            res.end("Upload failed");
+        });
+
+        req.on("end", () => {
+            console.log("Upload completed and connection closed");
+        });
+
+        req.on("close", () => {
+            console.log("Request stream closed by client");
+        });
+        // const writeStream = createWriteStream(`./storage/${req.headers.filename}`);
+        // let count = 0;
+        // req.on("data", (chunk) => {
+        //   count++;
+        //   writeStream.write(chunk);
+        // });
+        // req.on("end", () => {
+        //   console.log("Chunks received:", count);
+        //   res.end("File uploaded successfully");
+        // });
+    }
+
 })
 
-async function serverDirectory(url, res){
+async function serverDirectory(url, res) {
     const itemList = await readdir(`./storage${url}`)
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Content-Type', 'application/json')
